c1b28345810c14d5363dc2289fe65837f9a6a797
Yukinosuke Takada <yukinosuke.tokyo@gmail.com>
2023-05-04 17:42:44 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function NameList() {
+  const [list, setList] = useState(["Jack", "Jill", "John"]);
+  const [name, setName] = useState("June");
+
+  const onAddName = () => {
+    setList([...list, name]);
+    setName("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <ur>
+        {list.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ur>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={onAddName}
+      >
+        Add Name
+      </button>
+    </div>
+  )
+}
+
+function App() {
+  function Counter() {
+    const [count, setCount] = useState(10);
+
+    function addOne() {
+      setCount(count + 1);
+    }
+
+    return (
+      <div className="App">
+        <button onClick={() => addOne()}>Click me {count}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    );
+  }
+
+  return (
+    <div>
+      <Counter />
+      <NameList />
+    </div>
   )
 }
 
